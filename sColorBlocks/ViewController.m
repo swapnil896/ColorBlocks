9e97ca9b6b17ffa592a21a390abaabc2fff1eb9d
Mac <mac@Macs-Mac-Pro.local>
2016-10-07 13:51:54 +0530

    Initial Commit

---
+++ sColorBlocks/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sColorBlocks
+//
+//  Created by Mac on 07/10/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
